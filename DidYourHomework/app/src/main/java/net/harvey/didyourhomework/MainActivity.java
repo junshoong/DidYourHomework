4ad60b0b44b8ece0843f54e83d56805b03052f80
Kandinell <kandinell@gmail.com>
2015-05-31 16:55:32 +0900

    getMonthEvent 메소드 생성

--- DidYourHomework/app/src/main/java/net/harvey/didyourhomework/MainActivity.java
+++ DidYourHomework/app/src/main/java/net/harvey/didyourhomework/MainActivity.java
@@ -23,28 +23,48 @@ public class MainActivity extends AppCompatActivity {
     private int mWeekViewType = TYPE_WEEK_VIEW;
     private WeekView mWeekView;
     private Calendar week = Calendar.getInstance();
-
+    private ArrayList<WeekViewEvent> getEvents= new ArrayList<WeekViewEvent>();
 
     WeekView.MonthChangeListener mMonthChangeListener = new WeekView.MonthChangeListener() {
+
         @Override
         public ArrayList<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
             // Populate the week view with some events.
-            ArrayList<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
-
-            Calendar startTime = Calendar.getInstance();
-            startTime.set(Calendar.HOUR_OF_DAY, 10);
-            startTime.set(Calendar.MINUTE, 30);
-            startTime.set(Calendar.MONTH, newMonth - 1);
-            startTime.set(Calendar.YEAR, newYear);
-            Calendar endTime = (Calendar) startTime.clone();
-            endTime.add(Calendar.HOUR, 2);
-            endTime.set(Calendar.MONTH, newMonth - 1);
-            WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
-            event.setColor(getResources().getColor(R.color.event_color_01));
-            events.add(event);
+            ArrayList<WeekViewEvent> events = getMonthEvent(newYear,newMonth);
+//
+//            Calendar startTime = Calendar.getInstance();
+//            startTime.set(Calendar.HOUR_OF_DAY, 13);
+//            startTime.set(Calendar.MINUTE, 00);
+//            startTime.set(Calendar.MONTH, newMonth - 1);
+//            startTime.set(Calendar.YEAR, newYear);
+//            Calendar endTime = (Calendar) startTime.clone();
+//            endTime.add(Calendar.HOUR, 1);
+//            endTime.set(Calendar.MONTH, newMonth - 1);
+//            WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
+//            event.setColor(getResources().getColor(R.color.event_color_01));
+//            events.add(event);
             return events;
         }
     };
+
+    private ArrayList<WeekViewEvent> getMonthEvent(int newYear, int newMonth) {
+        getEvents.clear();
+        if(newYear == 2015 && newMonth == 6){
+            Calendar startTime = Calendar.getInstance();
+            startTime.set(Calendar.HOUR_OF_DAY, 13);
+            startTime.set(Calendar.MINUTE, 00);
+            startTime.set(Calendar.DAY_OF_MONTH,1);
+            startTime.set(Calendar.MONTH, newMonth - 1);
+            startTime.set(Calendar.YEAR, newYear);
+            Calendar endTime = (Calendar) startTime.clone();
+            endTime.add(Calendar.HOUR, 1);
+            endTime.set(Calendar.MONTH, newMonth - 1);
+            WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
+            getEvents.add(event);
+        }
+        return getEvents;
+    }
+
     private WeekView.EventClickListener mEventClickListener = new WeekView.EventClickListener() {
         @Override
         public void onEventClick(WeekViewEvent event, RectF eventRect) {
